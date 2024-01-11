c331b1ebd16c6c798bc4f960f74c0ea1950e0485
Saranga siriwardhana <it21166792@my.sliit.lk>
2024-01-12 04:39:21 +0530

    test

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -3,6 +3,7 @@ import LeftBanner from "./LeftBanner";
 import RightBanner from "./RightBanner";
 const Banner = () => {
   return (
+    // Banner
     <section
       id='home'
       className='w-full pt-10 pb-20 flex flex-col gap-10 xl:gap-0 lgl:flex-row items-center border-b-[1px] font-titleFont border-b-black'
